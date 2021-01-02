c035ab8bc6a329f34aa30fd0ed94e3026f1d0846
Samuel Karlström <sam.karlstrm@gmail.com>
2021-01-02 21:19:04 +0100

    1st basic frontend version

---
+++ frontend/src/components/Chat.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Chat = () => {
+  return (
+    <div className="chat-box">
+      <h1>chat page!</h1>
+      <p> asd</p>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ frontend/src/components/Join.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Join = () => {
+  return (
+    <div className="join-box">
+      <p>Join page!</p>
+      <p> asd</p>
+    </div>
+  );
+};
+
+export default Join;
